a0b7a3ad97d4d1aa4406a2cb307ab1b2da0c60fa
Bhavesh <bhavesh33mar@gmail.com>
2024-06-28 10:54:12 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MychartComponent } from './component/mychart/mychart.component';
 import { ChartDialogComponent } from './component/chart-dialog/chart-dialog.component';
-import { MAT_DIALOG_DEFAULT_OPTIONS, MatDialogModule  } from '@angular/material/dialog';
+import {  MatDialogModule  } from '@angular/material/dialog';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
